21ea2fb29ceb0a2025a5ee382207067a9b64a2f4
Ruma <77885035+Ruma-Jaman@users.noreply.github.com>
2023-10-17 10:21:06 +0600

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,65 @@
+<?php
+
+if (isset($_REQUEST['sbmt'])) {
+    $fname=$_REQUEST['fname'];
+    $lname=$_REQUEST['lname'];
+    $addrs=$_REQUEST['addrs'];
+    $city=$_REQUEST['city'];
+    $zipcode=$_REQUEST['zipcode'];
+    $sbmt=$_REQUEST['sbmt'];
+
+    $json_array=array('fname'=>$fname,'lname'=>$lname,'address'=>$addrs,'city'=>$city,'zipcode'=>$zipcode);
+    $json_format=json_encode($json_array,JSON_PRETTY_PRINT);
+
+    if (file_put_contents('data.json',$json_format)) {
+
+
+        echo "Successfully Written in data.json";
+    }else {
+        echo "Something Error!"
+    }
+   
+   
+} 
+
+?>
+<!DOCTYPE html>
+<html >
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Registration Form</h1>
+    <form action="" method="Post">
+    <table>
+        <tr >
+            <td>First Name:</td>
+            <td><input type="text"  name="fname">
+           </td>
+        </tr>
+        <tr>
+            <td>Last Name:</td>
+            <td><input type="text" name="lname"> </td>
+        </tr>
+        <tr>
+            <td>Address:</td>
+            <td><input type="text" name="addrs"> </td>
+        </tr>
+        <tr>
+            <td>City:</td>
+            <td><input type="text" name="city"> </td>
+        </tr>
+        <tr>
+            <td>Zip Code:</td>
+            <td><input type="number" name="zipcode"> </td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="Submit" name="sbmt"></td>
+        </tr>
+    </table>
+    </form>
+   
+</body>
+</html>
+
+?>
